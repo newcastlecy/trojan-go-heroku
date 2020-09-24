bbd5f587f0949073ea36acd594aa5a3d0579d50d
starts.sh <67519157+starts-sh@users.noreply.github.com>
2020-09-24 19:18:47 +0800

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -3,11 +3,6 @@
 # config trojan
 cat << EOF > /etc/trojan-go/config.json
 {
-    "run_type": "server",
-    "local_addr": "0.0.0.0",
-    "local_port": 443,
-    "remote_addr": "127.0.0.1",
-    "remote_port": 80,
     "password": [
         "$PASSWORD"
     ],
